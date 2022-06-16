e660089c542a82f2d7437d22ab20da00b9d3c54a
Nick Ciolpan <nick@graffino.com>
2022-06-17 00:43:15 +0300

    chore: removed unused figjam code logic
    Signed-off-by: Nick Ciolpan <nick@graffino.com>

--- code.ts
+++ code.ts
@@ -83,146 +83,92 @@ const generatePreview = (msg) => {
 }
 
 
-// Runs this code if the plugin is run in Figma
-if (figma.editorType === 'figma') {
-  // This plugin will open a window to prompt the user to enter a number, and
-  // it will then create that many rectangles on the screen.
+// This plugin will open a window to prompt the user to enter a number, and
+// it will then create that many rectangles on the screen.
 
-  // This shows the HTML page in "ui.html".
-  figma.showUI(__html__);
-  figma.ui.resize(300, 300);
-  // Calls to "parent.postMessage" from within the HTML page will trigger this
-  // callback. The callback will be passed the "pluginMessage" property of the
-  // posted message.
-  figma.ui.onmessage = async msg => {
-    // One way of distinguishing between different types of messages sent from
-    // your HTML page is to use an object with a "type" property like this.
-    if (msg.type === 'fetch-pages') {
-      const pages = figma.root.children;
-      figma.ui.postMessage({type: 'pages',figma: figma,  pages: pages.map(r => ({title: r.name, id: r.id}))});
+// This shows the HTML page in "ui.html".
+figma.showUI(__html__);
+figma.ui.resize(300, 300);
+// Calls to "parent.postMessage" from within the HTML page will trigger this
+// callback. The callback will be passed the "pluginMessage" property of the
+// posted message.
+figma.ui.onmessage = async msg => {
+  // One way of distinguishing between different types of messages sent from
+  // your HTML page is to use an object with a "type" property like this.
+  if (msg.type === 'fetch-pages') {
+    const pages = figma.root.children;
+    figma.ui.postMessage({type: 'pages',figma: figma,  pages: pages.map(r => ({title: r.name, id: r.id}))});
+  }
+
+  if (msg.type === 'get-current-source') {
+    figma.ui.postMessage({
+      type: 'retrieved-current-source', 
+      source: await figma.clientStorage.getAsync('source')
+    });
+  }
+
+  if (msg.type === 'update-source') {
+    try { 
+      latestPreview.remove();
+    } catch (e) {
+      latestPreview = null;
     }
 
-    if (msg.type === 'get-current-source') {
-      figma.ui.postMessage({
-        type: 'retrieved-current-source', 
-        source: await figma.clientStorage.getAsync('source')
-      });
-    }
+    latestPreview = null;
+    await figma.clientStorage.setAsync('source', msg.source);
+    const results = findAll(msg.componentName, msg.source, msg.level);
 
-    if (msg.type === 'update-source') {
-      try { 
+    if (results.length > 0) {
+      generatePreview(msg);
+      generateResults(msg);
+    } else  {
+      generateResults(msg);
+    }
+  }
+
+  if (msg.type === 'fetch-results') {
+    generateResults(msg);
+  }
+
+  if (msg.type === 'preview:remove') {
+    if (!!latestPreview) {
+      try {
         latestPreview.remove();
       } catch (e) {
         latestPreview = null;
       }
+    }
 
+    latestPreview = null;
+  }
+
+  if (msg.type === 'preview:insert') {
+    // The node is already insered at this point
+    // We just need to close the plugin
+    if (!!latestPreview) {
       latestPreview = null;
-      await figma.clientStorage.setAsync('source', msg.source);
-      const results = findAll(msg.componentName, msg.source, msg.level);
-
-      if (results.length > 0) {
-        generatePreview(msg);
-        generateResults(msg);
-      } else  {
-        generateResults(msg);
-      }
-    }
-
-    if (msg.type === 'fetch-results') {
-      generateResults(msg);
-    }
-
-    if (msg.type === 'preview:remove') {
-      if (!!latestPreview) {
-        try {
-          latestPreview.remove();
-        } catch (e) {
-          latestPreview = null;
-        }
-      }
-
-      latestPreview = null;
-    }
-
-    if (msg.type === 'preview:insert') {
-      // The node is already insered at this point
-      // We just need to close the plugin
-      if (!!latestPreview) {
-        latestPreview = null;
-        figma.closePlugin();
-      }
-    }
-
-    if (msg.type === 'preview') {
-      generatePreview(msg);
-    }
-
-    if (msg.type === 'insert-component') {
-      const node = findOne(msg.componentName, msg.source, msg.level)
-      node.visible = true;
-      const clone = node.clone();
-
-      figma.currentPage.selection = [clone];
-      figma.viewport.scrollAndZoomIntoView([clone]);
-    }
-
-    if (msg.type === 'cancel') {
       figma.closePlugin();
     }
+  }
 
+  if (msg.type === 'preview') {
+    generatePreview(msg);
+  }
 
-    // Make sure to close the plugin when you're done. Otherwise the plugin will
-    // keep running, which shows the cancel button at the bottom of the screen.
-  };
+  if (msg.type === 'insert-component') {
+    const node = findOne(msg.componentName, msg.source, msg.level)
+    node.visible = true;
+    const clone = node.clone();
 
-// If the plugins isn't run in Figma, run this code
-} else {
-  // This plugin will open a window to prompt the user to enter a number, and
-  // it will then create that many shapes and connectors on the screen.
+    figma.currentPage.selection = [clone];
+    figma.viewport.scrollAndZoomIntoView([clone]);
+  }
 
-  // This shows the HTML page in "ui.html".
-  figma.showUI(__html__);
-
-  // Calls to "parent.postMessage" from within the HTML page will trigger this
-  // callback. The callback will be passed the "pluginMessage" property of the
-  // posted message.
-  figma.ui.onmessage = msg => {
-    // One way of distinguishing between different types of messages sent from
-    // your HTML page is to use an object with a "type" property like this.
-    if (msg.type === 'insert-node') {
-      const numberOfShapes = msg.count;
-      const nodes: SceneNode[] = [];
-      for (let i = 0; i < numberOfShapes; i++) {
-        const shape = figma.createShapeWithText();
-        // You can set shapeType to one of: 'SQUARE' | 'ELLIPSE' | 'ROUNDED_RECTANGLE' | 'DIAMOND' | 'TRIANGLE_UP' | 'TRIANGLE_DOWN' | 'PARALLELOGRAM_RIGHT' | 'PARALLELOGRAM_LEFT'
-        shape.shapeType = 'ROUNDED_RECTANGLE'
-        shape.x = i * (shape.width + 200);
-        shape.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-        figma.currentPage.appendChild(shape);
-        nodes.push(shape);
-      };
-
-      for (let i = 0; i < (numberOfShapes - 1); i++) {
-        const connector = figma.createConnector();
-        connector.strokeWeight = 8
-
-        connector.connectorStart = {
-          endpointNodeId: nodes[i].id,
-          magnet: 'AUTO',
-        };
-
-        connector.connectorEnd = {
-          endpointNodeId: nodes[i+1].id,
-          magnet: 'AUTO',
-        };
-      };
-
-      figma.currentPage.selection = nodes;
-      figma.viewport.scrollAndZoomIntoView(nodes);
-    }
-
-    // Make sure to close the plugin when you're done. Otherwise the plugin will
-    // keep running, which shows the cancel button at the bottom of the screen.
+  if (msg.type === 'cancel') {
     figma.closePlugin();
-  };
+  }
+
+
+  // Make sure to close the plugin when you're done. Otherwise the plugin will
+  // keep running, which shows the cancel button at the bottom of the screen.
 };
